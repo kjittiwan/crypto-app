93e21a823565fb0c6eb3338f6ff0253c1e7a7111
Kann Jittiwan <kjittiwan2@gmail.com>
2023-05-29 14:04:57 +0700

    responsive details page

--- src/routes/CoinDetails.jsx
+++ src/routes/CoinDetails.jsx
@@ -43,12 +43,12 @@ export default function CoinDetails() {
   }
   const data = coin?.market_data.sparkline_7d.price
   return (
-    <section className="bg-secondary w-screen lg:h-screen pb-[5rem] px-10">
+    <section className="bg-secondary w-screen lg:h-screen lg:pb-[5rem] px-10">
       {loading ? (<CircularProgress />) : (
-        <div className="h-full flex gap-x-14   rounded-lg py-10 px-10">
-          <div className="flex flex-col justify-center w-[40%] px-10 gap-y-6 h-full bg-grey rounded-lg">
-            <img src={coin?.image.large} alt={coin?.name} className='min-h-[200px] max-w-[200px] self-center'/>
-            <div className="text-4xl font-bold self-center flex gap-x-4 items-center">{coin?.name}
+        <div className="h-full flex flex-col lg:flex-row gap-x-14 rounded-lg lg:py-10 pb-10 lg:px-10">
+          <div className="flex flex-col justify-center w-full lg:w-[40%] px-4 py-10 lg:px-10 gap-y-6 h-full bg-grey rounded-lg mb-10">
+            <img src={coin?.image.large} alt={coin?.name} className='min-h-[150px] max-w-[150px] lg:min-h-[200px] lg:max-w-[200px] self-center'/>
+            <div className="text-2xl lg:text-4xl font-bold self-center flex gap-x-4 items-center">{coin?.name}
             {user?.email 
             ? (
               <div>
@@ -62,9 +62,9 @@ export default function CoinDetails() {
             }
             
             </div>
-            {parse(`<p className="font-light text-subtitle text-lg text-center">${coin?.description.en.split(". ")[0]}</p>`)}
+            {parse(`<p className="font-light text-subtitle lg:text-lg text-center">${coin?.description.en.split(". ")[0]}</p>`)}
           </div>
-          <div className='flex flex-col gap-y-10 flex-1 justify-between '>
+          <div className='flex flex-col gap-y-10 flex-1 w-full lg:w-[50%] justify-between '>
             <div className="flex flex-col flex-1 bg-grey px-4 py-10 gap-y-10 rounded-lg">
               <div className='text-2xl font-semibold text-right'>Last 7 days</div>
               <Sparklines data={data}>
@@ -72,17 +72,17 @@ export default function CoinDetails() {
               </Sparklines> 
             </div>
             
-            <div className="font-light text-xl flex justify-between flex-1 w-full px-10 items-center bg-grey rounded-lg">
+            <div className="font-light text-base lg:text-xl flex justify-between lg:flex-row flex-col flex-1 w-full gap-y-4 lg:px-10 items-center py-6 lg:py-0 bg-grey rounded-lg">
               <div>
-                <span className="font-bold mr-3">
+                <span className="font-bold  mr-2">
                 Rank:</span> {coin?.market_cap_rank}
               </div>
               <div>
-                <span className=" font-bold mr-3">
+                <span className=" font-bold  mr-2">
                   Current Price:</span> {symbol} {coin?.market_data.current_price[`${currency}`].toLocaleString('en-US',{minimumFractionDigits: 2})}
               </div>
               <div>
-                <span className=" font-bold mr-3">Market Cap:</span> {symbol} {parseInt(coin?.market_data.market_cap[`${currency}`].toString().slice(0,-6)).toLocaleString('en-US')} M
+                <span className=" font-bold  mr-2">Market Cap:</span> {symbol} {parseInt(coin?.market_data.market_cap[`${currency}`].toString().slice(0,-6)).toLocaleString('en-US')} M
               </div>
             </div>
           </div>
